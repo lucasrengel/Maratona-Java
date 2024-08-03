db70d91e2fab373ec7297c4ac5f5d38f85d26fcd
lucasrengel <lucas_rengel@estudante.sc.senai.br>
2024-08-02 21:28:10 -0300

    Inicio do projeto

---
+++ src/main/java/academy/lucas/maratonajava/introducao/Aula01OlaMundo.java
@@ -0,0 +1,8 @@
+package academy.lucas.maratonajava.introducao;
+
+public class Aula01OlaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+    }
+}
